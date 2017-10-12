862acbca0537aa3522fa36e35ae06db92f3db2db
Eric Almanza <ea15d@my.fsu.edu>
2017-10-12 00:29:07 -0400

    Added info on 'srand' function source

--- Weapon.cpp
+++ Weapon.cpp
@@ -12,6 +12,8 @@ std::string Weapon::getName() {
     return name;
 }
 
+// used cplusplus.com to obtain info on srand vvv
+// http://www.cplusplus.com/reference/cstdlib/srand/
 double Weapon::hit() {
   srand(time(NULL));
     return hitPoints;
